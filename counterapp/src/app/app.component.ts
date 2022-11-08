478aea9fabf2d6424c3414dd63bf0f249d381481
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2022-11-08 22:44:23 +0530

    basic apps

---
+++ counterapp/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'counterapp';
+  counter = 0;
+
+  resetbtn(){
+    this.counter = 0
+  }
+  incrbtn(){
+    this.counter = this.counter+1;
+  }
+  decrbtn(){
+    this.counter = this.counter-1
+    if(this.counter <0){
+      this.counter = 0;
+    }
+  }
+}
---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo-list';
+  // constructor(){
+    // setTimeout(() => {
+    //   this.title = "Todo-List using Angular"
+    // }, 2000);
+  // }
+}
